b93069c0051e7f088ba5f17c676103414589b66b
adnstyle <sjchoi@adnstyle.com>
2023-04-12 11:37:22 +0900

    dd

---
+++ src/main/java/com/example/test12345678/Test12345678Application.java
@@ -0,0 +1,13 @@
+package com.example.test12345678;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test12345678Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Test12345678Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/test12345678/Test12345678ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.test12345678;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Test12345678ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
